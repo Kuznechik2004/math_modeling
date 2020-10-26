3bf682b297dd04979d53ac3f8333d6a71bf8f520
Kuznechik2004 <71905152+Kuznechik2004@users.noreply.github.com>
2020-10-26 18:44:30 +0200

    will done

---
+++ will done.py
@@ -0,0 +1,16 @@
+import math
+def plK(R):
+     x=math.pi*(R**2)
+     return x
+def plP(a, b):
+     x=a*b
+     return x
+def plT(a,b,c):
+     h=(a+b+c)/2
+     x=(a*h)/2
+     return x
+print('Круг 1')
+print('Прямоуголник 2')
+print('Треугольник 3')
+E=int(input('Выберите фигуру'))
+if E==     
