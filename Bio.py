2ae09b393c308359b861db6dbd4a43a0d7d9c6ec
Kuznechik2004 <71905152+Kuznechik2004@users.noreply.github.com>
2021-02-01 17:09:50 +0200

    Bio

---
+++ Bio.py
@@ -0,0 +1,21 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+t = np.arange(0, 5, 1)
+
+def bk_function(m, t):
+    dm=k*m
+    return dm
+
+m_0=10
+k=1.125
+   
+solve_Bi = odeint(bk_function, m_0, t)
+plt.plot(t, solve_Bi[:, 0], label='Увеличение бактерий')
+plt.xlabel('Скорость размножения, секунды')
+plt.ylabel('Количество бактерий')
+pit.title('Скорость размножения бактерий')
+plt.legend()
+
+plt.show()
